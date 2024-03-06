1167ae82ec50efcb9c481dbad6c0e03f5cb1eafd
lai2-24 <lai2-24@itismeucci.com>
2024-03-06 09:13:48 +0000

    tabella visualizzabile

--- adding-components/src/app/app.component.ts
+++ adding-components/src/app/app.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { Person } from './model/person';
 import { CommonModule } from '@angular/common';
+import {TabellaComponent} from './tabella/tabella.component';
 import { TestComponent } from './test/test.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, CommonModule, TestComponent],
+  imports: [RouterOutlet, FormsModule, CommonModule, TestComponent, TabellaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   
+  
+
+
 }
 
---
+++ adding-components/src/app/model/person.ts
@@ -0,0 +1,13 @@
+export class Person {
+
+    name: string
+    surname: string
+    age: string
+
+    constructor(name:string, surname:string, age:string) {
+
+        this.name = name
+        this.surname = surname
+        this.age = age
+    }
+}
